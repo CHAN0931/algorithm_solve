59737335def7bba0a709bcfc9649d9fa1fb371b9
chan <ashrky143@gmail.com>
2021-05-30 20:47:38 +0900

    Create 11720.java

---
+++ acmicpc/11720.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] s = new String[n];
+        s = br.readLine().split("");
+        br.close();
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += Integer.parseInt(s[i]);
+        }
+        System.out.println(sum);
+    }
+}
